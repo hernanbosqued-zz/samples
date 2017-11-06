8c3ae5051f298f7fa20c0177f58ff41d3d05cea9
Hernan Bosqued <hernan.bosqued@olx.com>
2017-11-06 17:42:49 -0300

    basic example

--- dagger/src/main/java/com/hernan/pruebas/MainActivity.java
+++ dagger/src/main/java/com/hernan/pruebas/MainActivity.java
@@ -1,13 +1,22 @@
 package com.hernan.pruebas;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String fragmentTag = "main_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_layout);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(fragmentTag);
+
+        if (fragment == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new MainFragment(), fragmentTag).commit();
+        }
     }
 }
